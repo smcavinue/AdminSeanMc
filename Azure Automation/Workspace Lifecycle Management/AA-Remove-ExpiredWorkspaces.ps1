3b7ad87596ec9241cdf29bcd9dd786a09e88d602
Sean McAvinue <smcavinue@gmail.com>
2023-11-06 07:41:27 +0000

    Final for "Remove-ExpiredWorkspace"

--- Azure Automation/Workspace Lifecycle Management/AA-Remove-ExpiredWorkspaces.ps1
+++ Azure Automation/Workspace Lifecycle Management/AA-Remove-ExpiredWorkspaces.ps1
@@ -10,7 +10,11 @@ Connect-MgGraph -Identity -NoWelcome
 Connect-PnPOnline -ManagedIdentity -Url $adminURL
 
 ##Get all sites that are marked for deletion
-[array]$Sites = Get-MgSiteListItem -SiteId $SiteID -ListId $listID -Filter "fields/MarkedforDeletion eq 'Yes'"  -Expand "fields(`$select=SiteURL)" -All -Select id,fields
+[array]$Sites = Get-MgSiteListItem -SiteId $SiteID -ListId $listID -Filter "fields/MarkedforDeletion eq 'Yes' and fields/Deletedon eq null"  -Expand "fields(`$select=SiteURL)" -All -Select id, fields
+
+if(!$sites){
+    write-output "No sites to remove"
+}
 
 $Fields = @{
     fields = @{    
@@ -21,11 +25,29 @@ $Fields = @{
 
 ##Delete each site
 ForEach ($Site in $Sites) {
+write-output "Deleting: $($site.Fields.AdditionalProperties.SiteURL)"
+    ##Check if there is an associated group
+    $siteRelativeURL = $site.Fields.AdditionalProperties.SiteURL.replace("https://", "").replace("/sites/", ":/sites/")
+    $SiteObject = Get-MgSite -SiteId $siteRelativeURL
+    $GroupObject = get-mggroup -Filter "displayName eq '$($SiteObject.displayName)'" -ErrorAction silentlycontinue
 
-    ##Delete site
-    Remove-PnPTenantSite -Url $SiteURL -Force -confirm:$false
+    ##If a group exists, delete the group, otherwise delete the site
+    if ($GroupObject) {
+        ##Delete the group
+        write-output "Site is a group connected site: $($site.Fields.AdditionalProperties.SiteURL)"
+        Remove-MgGroup -GroupId $GroupObject.id -Confirm:$false
+    }
+    else {
+        ##Delete site
+        write-output "Site is a communication site: $($site.Fields.AdditionalProperties.SiteURL)"
+        Remove-PnPTenantSite -Url $site.Fields.AdditionalProperties.SiteURL -Force
+    }
 
     ##Update list item to reflect that the site has been deleted
     Update-MgSiteListItem -SiteId $SiteID -ListId $ListID -ListItemId $Site.Id -BodyParameter $Fields
 }
 
+##Disconnect from PNP Online
+Disconnect-PnPOnline
+##Disconnect from Microsoft Graph
+Disconnect-MgGraph
