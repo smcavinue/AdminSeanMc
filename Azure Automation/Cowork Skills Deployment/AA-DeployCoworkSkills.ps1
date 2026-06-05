7c63eeda7e5ab53640ec7dc13b5371bf12b5e919
smcavinue <smcavinue@gmail.com>
2026-06-05 13:21:45 +0100

    Update

--- Azure Automation/Cowork Skills Deployment/AA-DeployCoworkSkills.ps1
+++ Azure Automation/Cowork Skills Deployment/AA-DeployCoworkSkills.ps1
@@ -8,23 +8,38 @@ Function CopyFileToOneDrive {
         [string]$TenantName,
         [string]$SiteName,
         [string]$TargetUserName,
-        [string]$SourceFilePath
+        [string]$SourceFileId
     )
 
+    $SourceFile = Get-MgDriveItem -DriveId $SourceDriveId -DriveItemId $SourceFileId
+
     $TargetOneDrive = get-mguserdrive -UserId sean.mcavinue@seanmcavinue.net | ? { $_.Name -eq "OneDrive" }
-    $TargetFilePath = "/drives/$($TargetOneDrive.Id)/root:/Documents/Cowork/$($SourceFilePath.Split('\')[-1])"
-    copy-mgdriveitem -DriveId $TargetOneDrive.Id -ItemId "root:/Documents/Cowork/$($SourceFilePath.Split('\')[-1])" -SourceFilePath $SourceFilePath
-    #Write-Host "Copying file to $($TargetUserName)'s OneDrive at $TargetFilePath"
-    #Invoke-MgGraphRequest -Method POST -Uri "https://graph.microsoft.com/v1.0$TargetFilePath:/copy" -Body @{
-    #    parentReference = @{
-    #        driveId = $OneDriveSite.Id
-    #    }
-    #    name = $SourceFilePath.Split('\')[-1]
-    #} | Out-Null
+    $TargetFilePath = "/drives/$($TargetOneDrive.Id)/root:/Documents/Cowork/Skills"
+    Try{
+    $TargetCoworkFolder = get-mgdriveitem -DriveId $TargetOneDrive.Id -DriveItemId "root:/Documents/Cowork1"
+    }catch{
+        $DocumentsFolder = Get-MgDriveItem -DriveId $TargetOneDrive.Id -DriveItemId "root:/Documents"
+        #Write-Host "Creating Cowork folder in $($TargetUserName)'s OneDrive"
+        $TargetCoworkFolder = New-MgDriveItemChild -DriveId $TargetOneDrive.Id -Name "Cowork1" -Folder @{ childCount = 0 } -DriveItemId  $DocumentsFolder.Id
+        $TargetSkillsFolder = New-MgDriveItemChild -DriveId $TargetOneDrive.Id -Name "Skills" -Folder @{ childCount = 0 } -DriveItemId  $TargetCoworkFolder.Id
 
+    }
 
+    Try{
+    $TargetSkillsFolder = get-mgdriveitem -DriveId $TargetOneDrive.Id -DriveItemId "root:/Documents/Cowork1/Skills"
+    }catch{
+        $TargetSkillsFolder = New-MgDriveItemChild -DriveId $TargetOneDrive.Id -Name "Skills" -Folder @{ childCount = 0 } -DriveItemId  $TargetCoworkFolder.Id
+    }
 
+    $params = @{
+        parentReference = @{
+            driveId = $TargetOneDrive.Id
+            id = $TargetSkillsFolder.Id
+        }
+        name = $SourceFile.Name
+    }
 
+    Copy-MgDriveItem -DriveId $SourceFile.ParentReference.DriveId -DriveItemId $SourceFile.Id -BodyParameter $params
 
 }
 
@@ -48,7 +63,7 @@ $PeopleList = get-mgsiteList -SiteId $CoworkSite.Id -Filter "displayName eq 'Use
 foreach ($Deployment in $Deployments) {
 
     $file = Invoke-MgGraphRequest -method Get -Uri "https://graph.microsoft.com/v1.0/sites/$($Coworksite.Id)/lists/$($List.Id)/items/$($deployment.id)/driveitem"
-
+    $sourceFilePath = $file.parentReference.path.Replace("/sites/$SiteName/drive/root:", "") + "/" + $file.name
     $User = Invoke-MgGraphRequest -method Get -Uri "https://graph.microsoft.com/v1.0/sites/$($Coworksite.Id)/lists/$($PeopleList.Id)/items/$($Deployment.fields.DistributeToLookupId)?expand=fields"
     
     If ($Deployment.fields.DistributeTo -eq "Everyone except external users") {
